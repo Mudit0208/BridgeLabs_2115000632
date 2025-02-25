c46e2032ba7a5b68c41d5ee849b8ec12137cb5c0
Mudit0208 <mudit.jadonn@gmail.com>
2025-02-25 23:50:12 +0530

    added

---
+++ EncryptCSV.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+import java.util.Base64;
+
+public class EncryptCSV {
+    public static void main(String[] args) throws IOException {
+        String inputFile = "employees.csv";
+        String outputFile = "encrypted_employees.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
+
+            String header = br.readLine();
+            bw.write(header + "\n");
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                data[3] = Base64.getEncoder().encodeToString(data[3].getBytes());
+                bw.write(String.join(",", data) + "\n");
+            }
+        }
+        System.out.println("Data encrypted.");
+    }
+}
---
+++ ReadCSV.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+
+public class ReadCSV {
+    public static void main(String[] args) {
+        String filePath = "students.csv";
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                System.out.printf("ID: %s, Name: %s, Age: %s, Marks: %s%n", data[0], data[1], data[2], data[3]);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
